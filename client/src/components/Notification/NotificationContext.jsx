f82eaf4608bf0b97cdbbc2f638f300bb75d150e0
elahe-fakour <elahe.fakoor16@gmail.com>
2025-07-16 19:25:00 +0330

    Create a Reusable Notification Component
    ref #FAM-20

---
+++ client/src/components/Notification/NotificationContext.jsx
@@ -0,0 +1,16 @@
+
+import { createContext, useContext } from 'react';
+
+
+export const NotificationContext = createContext(null);
+
+
+export const useNotification = () => {
+  const context = useContext(NotificationContext);
+  if (!context) {
+    throw new Error('useNotification must be used within a NotificationProvider');
+  }
+  return context;
+};
+
+
